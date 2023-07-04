0226959bc4795cadf18f7f1406117ad726223c7c
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2023-07-04 13:26:14 -0500

    Created players

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const c = canvas.getContext('2d')
 console.log(canvas, c );
 canvas.width = 1024
 canvas.height = 576
-// canvas.style.backgroundColor = blue
-// c.fillRect(0,0,canvas.width, canvas.height)
+
+c.fillRect(0,0,canvas.width, canvas.height)

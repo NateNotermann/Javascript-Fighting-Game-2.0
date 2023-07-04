70b6c4c279720c863f5742e8ffeb8c281c72c51f
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2023-07-04 13:42:05 -0500

    saving progress

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+// ---- Sprite Class ----
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
+        this.velocity = velocity
     }
     draw() {
         c.fillStyle = "red"
         c.fillRect(this.position.x, this.position.y, 50, 150)
     }
+    update() {
+        this.draw()
+        this.position.y += 10
+    }
 }
+
+// ---- Player 1 ----
 const player1 = new Sprite({
     position: {
         x: 0,
@@ -26,19 +34,28 @@ const player1 = new Sprite({
     }
 })
 
-player1.draw()
 
+// ---- Player 2 ----
 const player2 = new Sprite({
-    x: 400, 
-    y: 100
+    position: {
+        x: 400, 
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
 })
 
-player2.draw()
 
 console.log(player1);
 console.log(player2);
 
 function animate(){
     window.requestAnimationFrame(animate)
-    console.log("animate function running!");
+    // c.fillStyle = 'black'
+    // c.fillRect(0, 0, canvas.width, canvas.height)
+    // c.clearRect(0, 0, canvas.width, canvas.height)
+    player1.update()
+    player2.update()
 }

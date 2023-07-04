c511a4fcce784e5aa5a48d05cf10b08bce505890
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2023-07-04 13:27:46 -0500

    saving players

---
+++ ReadMe.md
@@ -0,0 +1,29 @@
+# Foobar
+
+Test project to make a 2D fighting game in .
+
+## Installation
+
+Use the package manager [google](ww.google) to install foobar. I was made using a YouTube tutorial by Chris Course. [GitHub](https://github.com/chriscourses) 
+
+[Tutorial](https://www.youtube.com/watch?v=vyqbNFMDRGQ&ab_channel=ChrisCourses).
+
+```bash
+npm install foobar
+```
+
+## List of Todo items
+Project Setup
+Create Player and Enemy
+Move Characters with event listeners
+Attacks
+Health Bar Interface
+Game Timers and Game Over
+
+
+## Contributing
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-console.log(canvas, c );
+console.log(canvas, c);
 canvas.width = 1024
 canvas.height = 576
 
-c.fillRect(0,0,canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+    }
+    draw() {
+        c.fillStyle = "red"
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
+const player1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+player1.draw()
+
+const player2 = new Sprite({
+    x: 400, 
+    y: 100
+})
+
+player2.draw()
+
+console.log(player1);
+console.log(player2);
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    console.log("animate function running!");
+}
